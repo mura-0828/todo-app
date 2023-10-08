b9cbf03f2512a832972ac4a6229b307603b5cc6d
mura-0828 <workmura@icloud.com>
2023-10-08 19:58:41 +0900

    スタイリング

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,37 +16,45 @@ export default function Home() {
   }, [])
 
   return (
-    <div>
-      <h1>todo</h1>
+    <div className='container mx-auto p-4'>
+      <h1 className='text-3xl font-bold mb-4'>Todo</h1>
       {todos.map((todo) => (
-        <div key={todo.id}>
-          <p>{todo.title}</p>
-          <input
-            type='checkbox'
-            checked={todo.completed}
-            onChange={async () => {
-              const response = await fetch(
-                `${process.env.NEXT_PUBLIC_API_URL}/todo/${todo.id}`,
-                {
-                  method: 'PUT',
-                  headers: {
-                    'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({ completed: todo.completed }),
-                }
-              )
-              const updateTodo = await response.json()
-              setTodos(
-                todos.map((todo) => {
-                  if (todo.id === updateTodo.id) {
-                    return updateTodo
-                  } else {
-                    return todo
+        <div
+          key={todo.id}
+          className='flex items-center justify-between bg-gray-200 p-2 rounded mb-2'
+        >
+          <div className='flex items-center'>
+            <input
+              type='checkbox'
+              checked={todo.completed}
+              onChange={async () => {
+                const response = await fetch(
+                  `${process.env.NEXT_PUBLIC_API_URL}/todo/${todo.id}`,
+                  {
+                    method: 'PUT',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ completed: todo.completed }),
                   }
-                })
-              )
-            }}
-          />
+                )
+                const updateTodo = await response.json()
+                setTodos(
+                  todos.map((todo) => {
+                    if (todo.id === updateTodo.id) {
+                      return updateTodo
+                    } else {
+                      return todo
+                    }
+                  })
+                )
+              }}
+              className='mr-2'
+            />
+            <p className={`text-black ${todo.completed ? 'line-through' : ''}`}>
+              {todo.title}
+            </p>
+          </div>
           <button
             onClick={async (e) => {
               e.preventDefault()
@@ -59,6 +67,7 @@ export default function Home() {
               const deleteTodo = await response.json()
               setTodos(todos.filter((todo) => todo.id !== deleteTodo.id))
             }}
+            className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded'
           >
             削除
           </button>
@@ -83,14 +92,21 @@ export default function Home() {
           setTodos([...todos, newTodo])
           setInputValue(null)
         }}
+        className='flex items-center mt-4'
       >
         <input
           type='text'
-          className='text-black'
+          className='border border-gray-400 px-4 py-2 mr-2 rounded'
           value={inputValue || ''}
           onChange={(e) => setInputValue(e.target.value)}
+          placeholder='Todoを入力してください'
         />
-        <input type='submit' value='add' />
+        <button
+          type='submit'
+          className='bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded'
+        >
+          追加
+        </button>
       </form>
     </div>
   )
